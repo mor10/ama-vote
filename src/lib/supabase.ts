33965f596da1cc72c4251709f35763517998d07a
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2024-11-29 22:43:41 -0800

    fix bug

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
-import type { Question } from '../assets/types'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
